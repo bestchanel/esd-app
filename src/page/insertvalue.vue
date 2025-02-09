827a8a491f9f61c788a535e735b352be440c9f9e
bestchanel <samsunga77789@gmail.com>
2025-02-09 21:49:43 +0700

    fix pdf

--- src/page/insertvalue.vue
+++ src/page/insertvalue.vue
@@ -1074,7 +1074,7 @@ const SaveData = async () => {
 
     try {
       const response = await axios.post(
-        "https://esd-app-strapi-dq8t.onrender.com/api/wrist-straps",
+        "http://localhost:1337/api/wrist-straps",
         payload,
         {
           headers: {
@@ -1122,7 +1122,7 @@ const SaveData = async () => {
 
     try {
       const response = await axios.post(
-        "https://esd-app-strapi-dq8t.onrender.com/api/table-mats",
+        "http://localhost:1337/api/table-mats",
         payload,
         {
           headers: {
@@ -1168,7 +1168,7 @@ const selecttablenow = () => {
 
 const fetchBranches = async () => {
   try {
-    const response = await axios.get('https://esd-app-strapi-dq8t.onrender.com/api/branches');
+    const response = await axios.get('http://localhost:1337/api/branches');
     branches.value = response.data.data;
   } catch (error) {
     console.error('Error fetching branches:', error);
@@ -1192,7 +1192,7 @@ const get_data = () => {
   if (selectedTable.value == "TABLE MAT") {
     axios
       .get(
-        "https://esd-app-strapi-dq8t.onrender.com/api/table-mats?populate[users_permissions_user][populate]=branches"
+        "http://localhost:1337/api/table-mats?populate[users_permissions_user][populate]=branches"
       )
       .then((table_matsresponse) => {
         data_table_mats.value = table_matsresponse.data.data;
@@ -1205,7 +1205,7 @@ const get_data = () => {
   } else if (selectedTable.value == "WRIST STRAP") {
     axios
       .get(
-        "https://esd-app-strapi-dq8t.onrender.com/api/wrist-straps?populate[users_permissions_user][populate]=branches"
+        "http://localhost:1337/api/wrist-straps?populate[users_permissions_user][populate]=branches"
       )
       .then((wrist_strapsresponse) => {
         // wrist_straps.value = wrist_strapsresponse.data.data.map(item => item.attributes.Resistance.);
@@ -1264,7 +1264,7 @@ const UpdateItemWristStrap = async () => {
 
   try {
     await axios.put(
-      `https://esd-app-strapi-dq8t.onrender.com/api/wrist-straps/${dataupdatemodal.value.id}`,
+      `http://localhost:1337/api/wrist-straps/${dataupdatemodal.value.id}`,
       data_update,
       {
         headers: {
@@ -1309,7 +1309,7 @@ const UpdateItemTableMat = async () => {
 
   try {
     await axios.put(
-      `https://esd-app-strapi-dq8t.onrender.com/api/table-mats/${dataupdatemodal.value.id}`,
+      `http://localhost:1337/api/table-mats/${dataupdatemodal.value.id}`,
       data_update,
       {
         headers: {
@@ -1358,7 +1358,7 @@ const deleteItemWristStrap = async (id) => {
 
   if (confirmDelete.isConfirmed) {
     try {
-      await axios.delete(`https://esd-app-strapi-dq8t.onrender.com/api/wrist-straps/${id}`, {
+      await axios.delete(`http://localhost:1337/api/wrist-straps/${id}`, {
         headers: {
           Authorization: `Bearer ${token.value}`,
         },
@@ -1398,7 +1398,7 @@ const deleteItemTableMat = async (id) => {
 
   if (confirmDelete.isConfirmed) {
     try {
-      await axios.delete(`https://esd-app-strapi-dq8t.onrender.com/api/table-mats/${id}`, {
+      await axios.delete(`http://localhost:1337/api/table-mats/${id}`, {
         headers: {
           Authorization: `Bearer ${token.value}`,
         },
@@ -1523,7 +1523,7 @@ watch(savedSignatureTableMat, (newVal) => {
 /////////////////////////
 
 
-const pdfUrl = "public/HP-06-Wrist-Strap-Weekly-Self-Check-v.02.pdf";
+const pdfUrl = "/HP-06-Wrist-Strap-Weekly-Self-Check-v.02.pdf";
 
 const printWristStrap = async () => {
   try {
@@ -1717,7 +1717,7 @@ const printWristStrap = async () => {
 
 
 
-const pdfUrlTableMat = "public/HP-07-Table-Mat-Checklist-v.02.pdf";
+const pdfUrlTableMat = "/HP-07-Table-Mat-Checklist-v.02.pdf";
 
 const printTableMat = async () => {
   try {
